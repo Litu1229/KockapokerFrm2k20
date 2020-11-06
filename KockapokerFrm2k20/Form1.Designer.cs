63beac0b1e3aa5c3d52810d13108d3b0f23e4020
Ripperoni <lcsabi9d@gmail.com>
2020-11-06 13:12:35 +0100

    Add project files.

---
+++ KockapokerFrm2k20/Form1.Designer.cs
@@ -0,0 +1,279 @@
+﻿namespace KockapokerFrm2k20
+{
+    partial class FrmFo
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.lblGepReszEredmeny = new System.Windows.Forms.Label();
+            this.lblEmberReszEredmeny = new System.Windows.Forms.Label();
+            this.pbGep1 = new System.Windows.Forms.PictureBox();
+            this.pbGep2 = new System.Windows.Forms.PictureBox();
+            this.pbGep3 = new System.Windows.Forms.PictureBox();
+            this.pbGep4 = new System.Windows.Forms.PictureBox();
+            this.pbGep5 = new System.Windows.Forms.PictureBox();
+            this.pbEmber5 = new System.Windows.Forms.PictureBox();
+            this.pbEmber4 = new System.Windows.Forms.PictureBox();
+            this.pbEmber3 = new System.Windows.Forms.PictureBox();
+            this.pbEmber2 = new System.Windows.Forms.PictureBox();
+            this.pbEmber1 = new System.Windows.Forms.PictureBox();
+            this.btnDobas = new System.Windows.Forms.Button();
+            this.lblGeperedmeny = new System.Windows.Forms.Label();
+            this.lblEmbereredmeny = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pbGep1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbGep2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbGep3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbGep4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbGep5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbEmber5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbEmber4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbEmber3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbEmber2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbEmber1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(149, 62);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Gép";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(568, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(54, 17);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Ember";
+            // 
+            // lblGepReszEredmeny
+            // 
+            this.lblGepReszEredmeny.AutoSize = true;
+            this.lblGepReszEredmeny.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblGepReszEredmeny.Location = new System.Drawing.Point(137, 117);
+            this.lblGepReszEredmeny.Name = "lblGepReszEredmeny";
+            this.lblGepReszEredmeny.Size = new System.Drawing.Size(46, 17);
+            this.lblGepReszEredmeny.TabIndex = 2;
+            this.lblGepReszEredmeny.Text = "label3";
+            // 
+            // lblEmberReszEredmeny
+            // 
+            this.lblEmberReszEredmeny.AutoSize = true;
+            this.lblEmberReszEredmeny.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lblEmberReszEredmeny.Location = new System.Drawing.Point(567, 117);
+            this.lblEmberReszEredmeny.Name = "lblEmberReszEredmeny";
+            this.lblEmberReszEredmeny.Size = new System.Drawing.Size(46, 17);
+            this.lblEmberReszEredmeny.TabIndex = 3;
+            this.lblEmberReszEredmeny.Text = "label3";
+            // 
+            // pbGep1
+            // 
+            this.pbGep1.Location = new System.Drawing.Point(41, 160);
+            this.pbGep1.Name = "pbGep1";
+            this.pbGep1.Size = new System.Drawing.Size(50, 50);
+            this.pbGep1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbGep1.TabIndex = 4;
+            this.pbGep1.TabStop = false;
+            // 
+            // pbGep2
+            // 
+            this.pbGep2.Location = new System.Drawing.Point(97, 160);
+            this.pbGep2.Name = "pbGep2";
+            this.pbGep2.Size = new System.Drawing.Size(50, 50);
+            this.pbGep2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbGep2.TabIndex = 5;
+            this.pbGep2.TabStop = false;
+            // 
+            // pbGep3
+            // 
+            this.pbGep3.Location = new System.Drawing.Point(153, 160);
+            this.pbGep3.Name = "pbGep3";
+            this.pbGep3.Size = new System.Drawing.Size(50, 50);
+            this.pbGep3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbGep3.TabIndex = 6;
+            this.pbGep3.TabStop = false;
+            // 
+            // pbGep4
+            // 
+            this.pbGep4.Location = new System.Drawing.Point(209, 160);
+            this.pbGep4.Name = "pbGep4";
+            this.pbGep4.Size = new System.Drawing.Size(50, 50);
+            this.pbGep4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbGep4.TabIndex = 7;
+            this.pbGep4.TabStop = false;
+            // 
+            // pbGep5
+            // 
+            this.pbGep5.Location = new System.Drawing.Point(265, 160);
+            this.pbGep5.Name = "pbGep5";
+            this.pbGep5.Size = new System.Drawing.Size(50, 50);
+            this.pbGep5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbGep5.TabIndex = 8;
+            this.pbGep5.TabStop = false;
+            // 
+            // pbEmber5
+            // 
+            this.pbEmber5.Location = new System.Drawing.Point(675, 160);
+            this.pbEmber5.Name = "pbEmber5";
+            this.pbEmber5.Size = new System.Drawing.Size(50, 50);
+            this.pbEmber5.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbEmber5.TabIndex = 13;
+            this.pbEmber5.TabStop = false;
+            // 
+            // pbEmber4
+            // 
+            this.pbEmber4.Location = new System.Drawing.Point(619, 160);
+            this.pbEmber4.Name = "pbEmber4";
+            this.pbEmber4.Size = new System.Drawing.Size(50, 50);
+            this.pbEmber4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbEmber4.TabIndex = 12;
+            this.pbEmber4.TabStop = false;
+            // 
+            // pbEmber3
+            // 
+            this.pbEmber3.Location = new System.Drawing.Point(563, 160);
+            this.pbEmber3.Name = "pbEmber3";
+            this.pbEmber3.Size = new System.Drawing.Size(50, 50);
+            this.pbEmber3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbEmber3.TabIndex = 11;
+            this.pbEmber3.TabStop = false;
+            // 
+            // pbEmber2
+            // 
+            this.pbEmber2.Location = new System.Drawing.Point(507, 160);
+            this.pbEmber2.Name = "pbEmber2";
+            this.pbEmber2.Size = new System.Drawing.Size(50, 50);
+            this.pbEmber2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbEmber2.TabIndex = 10;
+            this.pbEmber2.TabStop = false;
+            // 
+            // pbEmber1
+            // 
+            this.pbEmber1.Location = new System.Drawing.Point(451, 160);
+            this.pbEmber1.Name = "pbEmber1";
+            this.pbEmber1.Size = new System.Drawing.Size(50, 50);
+            this.pbEmber1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbEmber1.TabIndex = 9;
+            this.pbEmber1.TabStop = false;
+            // 
+            // btnDobas
+            // 
+            this.btnDobas.Location = new System.Drawing.Point(342, 391);
+            this.btnDobas.Name = "btnDobas";
+            this.btnDobas.Size = new System.Drawing.Size(100, 50);
+            this.btnDobas.TabIndex = 14;
+            this.btnDobas.Text = "Dobás";
+            this.btnDobas.UseVisualStyleBackColor = true;
+            this.btnDobas.Click += new System.EventHandler(this.btnDobas_Click);
+            // 
+            // lblGeperedmeny
+            // 
+            this.lblGeperedmeny.AutoSize = true;
+            this.lblGeperedmeny.Location = new System.Drawing.Point(140, 372);
+            this.lblGeperedmeny.Name = "lblGeperedmeny";
+            this.lblGeperedmeny.Size = new System.Drawing.Size(35, 13);
+            this.lblGeperedmeny.TabIndex = 15;
+            this.lblGeperedmeny.Text = "label3";
+            // 
+            // lblEmbereredmeny
+            // 
+            this.lblEmbereredmeny.AutoSize = true;
+            this.lblEmbereredmeny.Location = new System.Drawing.Point(577, 372);
+            this.lblEmbereredmeny.Name = "lblEmbereredmeny";
+            this.lblEmbereredmeny.Size = new System.Drawing.Size(35, 13);
+            this.lblEmbereredmeny.TabIndex = 16;
+            this.lblEmbereredmeny.Text = "label3";
+            // 
+            // FrmFo
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.lblEmbereredmeny);
+            this.Controls.Add(this.lblGeperedmeny);
+            this.Controls.Add(this.btnDobas);
+            this.Controls.Add(this.pbEmber5);
+            this.Controls.Add(this.pbEmber4);
+            this.Controls.Add(this.pbEmber3);
+            this.Controls.Add(this.pbEmber2);
+            this.Controls.Add(this.pbEmber1);
+            this.Controls.Add(this.pbGep5);
+            this.Controls.Add(this.pbGep4);
+            this.Controls.Add(this.pbGep3);
+            this.Controls.Add(this.pbGep2);
+            this.Controls.Add(this.pbGep1);
+            this.Controls.Add(this.lblEmberReszEredmeny);
+            this.Controls.Add(this.lblGepReszEredmeny);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "FrmFo";
+            this.Text = "Kockapóker";
+            ((System.ComponentModel.ISupportInitialize)(this.pbGep1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbGep2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbGep3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbGep4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbGep5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbEmber5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbEmber4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbEmber3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbEmber2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbEmber1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label lblGepReszEredmeny;
+        private System.Windows.Forms.Label lblEmberReszEredmeny;
+        private System.Windows.Forms.PictureBox pbGep1;
+        private System.Windows.Forms.PictureBox pbGep2;
+        private System.Windows.Forms.PictureBox pbGep3;
+        private System.Windows.Forms.PictureBox pbGep4;
+        private System.Windows.Forms.PictureBox pbGep5;
+        private System.Windows.Forms.PictureBox pbEmber5;
+        private System.Windows.Forms.PictureBox pbEmber4;
+        private System.Windows.Forms.PictureBox pbEmber3;
+        private System.Windows.Forms.PictureBox pbEmber2;
+        private System.Windows.Forms.PictureBox pbEmber1;
+        private System.Windows.Forms.Button btnDobas;
+        private System.Windows.Forms.Label lblGeperedmeny;
+        private System.Windows.Forms.Label lblEmbereredmeny;
+    }
+}
+
---
+++ KockapokerFrm2k20/Form1.cs
@@ -0,0 +1,95 @@
+﻿using Kockapoker;
+using KockapokerFrm2k20.Properties;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KockapokerFrm2k20
+{
+    public partial class FrmFo : Form
+    {
+        private Dobas gep;
+        private Dobas ember;
+        private PictureBox[] gepkep;
+        private PictureBox[] emberkep;
+        public FrmFo()
+        {
+            InitializeComponent();
+            gepkep = new PictureBox[5] { pbGep1, pbGep2, pbGep3, pbGep4, pbGep5 };
+            emberkep = new PictureBox[5] { pbEmber1, pbEmber2, pbEmber3, pbEmber4, pbEmber5 };
+            gep = new Dobas();
+            ember = new Dobas();
+
+            lblEmberReszEredmeny.Text = "";
+            lblGepReszEredmeny.Text = "";
+            lblGeperedmeny.Text = "";
+            lblEmbereredmeny.Text = "";
+        }
+        private void KepElhelyez(PictureBox pb, int szam)
+        {
+            switch (szam)
+            {
+                case 1:
+                    pb.Image = Resources.egy;
+                    break;
+                case 2:
+                    pb.Image = Resources.ketto;
+                    break;
+                case 3:
+                    pb.Image = Resources.harom;
+                    break;
+                case 4:
+                    pb.Image = Resources.negy;
+                    break;
+                case 5:
+                    pb.Image = Resources.ot;
+                    break;
+                case 6:
+                    pb.Image = Resources.hat;
+                    break;
+                default:
+                    break;
+            }
+        }
+        private void DobasMegjelenit(Dobas d,PictureBox[] kepek)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                KepElhelyez(kepek[i], d.Kockak[i]);
+            }
+        }
+        private void btnDobas_Click(object sender, EventArgs e)
+        {
+            gep.EgyDobas();
+            ember.EgyDobas();
+            DobasMegjelenit(gep,gepkep);
+            DobasMegjelenit(ember,emberkep);
+
+            lblEmberReszEredmeny.Text = ember.Eredmeny;
+            lblGepReszEredmeny.Text = gep.Eredmeny;
+
+            if (gep.Pont > ember.Pont)
+            {
+                MessageBox.Show("Gép nyert!", "Játszott kör", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                gep.Nyert++;
+                lblGeperedmeny.Text = gep.Nyert.ToString();
+            }
+            else if (gep.Pont < ember.Pont)
+            {
+                MessageBox.Show("Ember nyert!", "Játszott kör", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                ember.Nyert++;
+                lblEmbereredmeny.Text = ember.Nyert.ToString();
+            }
+            else
+            {
+                MessageBox.Show("Döntetlen!", "Játszott kör", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+    }
+}
